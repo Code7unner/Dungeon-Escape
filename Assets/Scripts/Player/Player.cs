e806055fd010182fa648c10cfa040534099c77f8
Code7unner <cherbadzhimaxim@gmail.com>
2020-01-21 00:32:18 +1000

    Add Giant-enemy

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    private bool _resetJump = false;
+    [SerializeField]
+    private float _speed = 2.5f;
+    private bool _grounded = false;
+    private PlayerAnimation _playerAnim;
+    private SpriteRenderer _playerSprite;
+    private SpriteRenderer _swordArcSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // assign handle of rigidbody
+        _rigid = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        _swordArcSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetMouseButtonDown(0) && IsGrounded() == true) 
+        {
+            _playerAnim.Attack();
+        }
+    }
+
+    void Movement() 
+    {
+        // horizontal input for left/right
+        float move = Input.GetAxisRaw("Horizontal");
+        _grounded = IsGrounded();
+
+        if (move > 0) 
+        {
+            Flip(true);
+        }
+        else if (move < 0) 
+        {
+            Flip(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true) 
+        {
+            Debug.Log("Jump!");
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpNeededRoutine());
+            _playerAnim.Jump(true);
+        }        
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+
+        _playerAnim.Move(move);
+    }
+
+    void Flip(bool faceRight) 
+    {
+        if (faceRight == true) 
+        {
+            _playerSprite.flipX = false;
+            _swordArcSprite.flipX = false;
+            _swordArcSprite.flipY = false;
+
+            Vector3 newPos = _swordArcSprite.transform.localPosition;
+            newPos.x = 0.34f;
+            _swordArcSprite.transform.localPosition = newPos;
+        }
+        else 
+        {
+            _playerSprite.flipX = true;
+            _swordArcSprite.flipX = true;
+            _swordArcSprite.flipY = true;
+
+            Vector3 newPos = _swordArcSprite.transform.localPosition;
+            newPos.x = -0.34f;
+            _swordArcSprite.transform.localPosition = newPos;
+        }
+    }
+
+    bool IsGrounded() 
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
+
+        if (hitInfo.collider != null) 
+        {
+            if (_resetJump == false) 
+            {
+                _playerAnim.Jump(false);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    IEnumerator ResetJumpNeededRoutine() 
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
