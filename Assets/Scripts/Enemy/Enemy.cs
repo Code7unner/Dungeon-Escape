8c9d8bffc3dde71dea579b59fa5976c4d1d87cc4
Code7unner <cherbadzhimaxim@gmail.com>
2020-01-21 02:58:30 +1000

    Add Spider-enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,11 +12,54 @@ public abstract class Enemy : MonoBehaviour
     protected int gems;
     [SerializeField]
     protected Transform pointA, pointB;
-    
-    public virtual void Attack() 
+
+    protected Vector3 currentTarget;
+    protected Animator anim;
+    protected SpriteRenderer sprite;
+
+    public virtual void Init() 
     {
-        
+        anim = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
-    public abstract void Update();
+    private void Start() 
+    {
+        Init();
+    }
+
+    public virtual void Update() 
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
+        {
+            return;
+        }
+
+        Movement();
+    }
+    
+    public virtual void Movement() 
+    {
+        if (currentTarget == pointA.position) 
+        {
+            sprite.flipX = true;
+        }
+        else 
+        {
+            sprite.flipX = false;
+        }
+
+        if (transform.position == pointA.position) 
+        {
+            currentTarget = pointB.position;
+            anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position) 
+        {
+            currentTarget = pointA.position;
+            anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
 }
--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,47 +4,8 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    private Vector3 _currentTarget;
-    private Animator _anim;
-    private SpriteRenderer _mossSprite;
-    private void Start() 
+    public override void Init() 
     {
-        _anim = GetComponentInChildren<Animator>();
-        _mossSprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    public override void Update() 
-    {
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
-        {
-            return;
-        }
-
-        Movement();
-    } 
-
-    void Movement() 
-    {
-        if (_currentTarget == pointA.position) 
-        {
-            _mossSprite.flipX = true;
-        }
-        else 
-        {
-            _mossSprite.flipX = false;
-        }
-
-        if (transform.position == pointA.position) 
-        {
-            _currentTarget = pointB.position;
-            _anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position) 
-        {
-            _currentTarget = pointA.position;
-            _anim.SetTrigger("Idle");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+        base.Init();
     }
 } 
